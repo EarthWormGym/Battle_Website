eb5fd4f0e4707f19cc0359e3f5ec1f2d04213d97
eagle310 <schand9639@gmail.com>
2019-10-15 16:59:04 +0100

    adds player names

--- spec/features/testing_name_input_spec.rb
+++ spec/features/testing_name_input_spec.rb
@@ -4,10 +4,9 @@ require 'spec_helper'
 feature 'Testing name submits' do
   scenario 'can fill in player names and click submit button' do
     visit('/player-names')
-    fill_in 'post[player_1]', with 'Adam'
-    fill_in 'post[player_2]', with 'Sim'
-    find('Submit').click
-    expect(@player_1).to eq('Adam')
-    expect(@player_2).to eq('Sim')
+    fill_in :player_1, with: 'Adam'
+    fill_in :player_2, with: 'Sim'
+    click_button 'Submit'
+    expect(page).to have_content "player 1's name is:Adam, player 2's name is:Sim"
   end
 end
