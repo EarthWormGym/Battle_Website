fd916ca4a17402df66b17f2fa2f7ae5c207d275c
eagle310 <schand9639@gmail.com>
2019-10-15 16:09:45 +0100

    plafg

--- app.rb
+++ app.rb
@@ -6,5 +6,22 @@ class Battle < Sinatra::Base
     p "working"
   end
 
+
+  get '/' do
+   'Testing infrastructure working!'
+  end
+
+  get '/player-names' do
+    erb :index
+  end
+
+  post '/player-names' do
+    @player_1 = params[:player_1]
+    @player_2 = params[:player_2]
+    erb(:index)
+    print @player_1
+    print @player_2
+  end
+
   run! if app_file == $0
 end
