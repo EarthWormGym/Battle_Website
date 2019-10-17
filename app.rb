debf2fa705e19c724c6d6cc7fae54662e3a5b16e
adam.beresford <adam56.beresford@gmail.com>
2019-10-17 16:01:10 +0100

    player health points

--- app.rb
+++ app.rb
@@ -31,10 +31,7 @@ class Battle < Sinatra::Base
   get '/attack' do
     @player_1 = $player_1.name
     @player_2 = $player_2.name
+    # @player_1.attack(@player_2)
     erb :attack
   end
 end
-
-
-
-# run! if app_file == $0
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,6 +2,7 @@ require 'player'
 
 describe Player do
   subject(:adam) { Player.new('Adam') }
+  subject(:sim) { Player.new('Sim') }
 
   describe '#name' do
     it 'returns the name' do
@@ -11,8 +12,21 @@ describe Player do
 
   describe '#hit_points' do
     it 'returns the hit points' do
-      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+      expect(adam.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
   end
-  
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(sim).to receive(:receive_damage)
+      adam.attack(sim)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { adam.receive_damage }.to change { adam.hit_points }.by(-10)
+    end
+  end
+
 end
